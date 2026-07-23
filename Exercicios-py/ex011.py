d1d5629812a5d5a740dcae480df37c30858a334f
guilhermeduarte1-maker <guilherme.duarte1@escolar.ifrn.edu.br>
2026-07-23 18:27:01 -0300

    Questao 11

---
+++ Exercicios-py/ex011.py
@@ -0,0 +1,27 @@
+# ==========================================
+# EXERCÍCIO BÔNUS (11): Maior e Menor sem usar max/min
+#
+# O QUE FAZER:
+# Crie uma lista com 5 números inteiros.
+# Descubra qual é o MAIOR e qual é o MENOR número dessa lista
+# usando apenas um laço 'for' (sem usar as funções max() e min()).
+#
+# DICA: Crie duas variáveis 'maior' e 'menor' iniciando com o
+# primeiro item da lista: lista[0].
+# Depois compare elemento por elemento dentro do laço.
+# ==========================================
+
+# Escreva seu código aqui:
+num = [10, 5, 8, 3, 12]
+for i in range(len(num)):
+    if i == 0:
+        maior = num[i]
+        menor = num[i]
+    else:
+        if num[i] > maior:
+            maior = num[i]
+        if num[i] < menor:
+            menor = num[i]
+
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
