4fc2dc71c90d38eb784c66b24b670a39b478b90f
guilhermeduarte1-maker <guilherme.duarte1@escolar.ifrn.edu.br>
2026-07-23 19:22:01 -0300

    Mais questoes

---
+++ Exercicios-POO/ex001.py
@@ -0,0 +1,19 @@
+# ==========================================
+# EXERCÍCIO 12: Introdução à POO (Classe e Objeto)
+#
+# O QUE FAZER:
+# 1. Crie uma classe chamada 'Cachorro'.
+# 2. Crie um método chamado 'latir()' que exibe: "[Nome] está latindo: Au Au!"
+# ==========================================
+
+# Escreva seu código aqui:
+class Cachorro:
+    def __init__(self, nome, raca):
+        self.nome = nome
+        self.raca = raca
+
+    def latir(self):
+        return f"{self.nome} esta latindo: Au Au!"
+
+c1 = Cachorro("Rex", "Doberman")
+print(c1.latir())
